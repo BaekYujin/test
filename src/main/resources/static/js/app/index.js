775b04df282ba3a16c0fcce59ac2d75c2c938751
BaekYujin <byujin66@gmail.com>
2020-05-19 21:48:09 +0900

    mustache 사용하여 view 생성 - 게시글 수정/삭제 기능 (20200519)

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,8 +1,16 @@
-var index = {
+var main = {
     init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
+        });
+
+        $('#btn-update').on('click' , function(){
+            _this.update();
+        });
+
+        $('#btn-delete').on('click' , function(){
+            _this.delete();
         });
 
     },
@@ -12,7 +20,7 @@ var index = {
             author: $('#author').val(),
             content: $('#content').val()
         };
-    console.log("test");
+
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
@@ -27,7 +35,44 @@ var index = {
         });
     },
 
+    update : function(){
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/'; //글이 등록이 성공하면 메인페이지로(/)로 이동
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function(){
+        var id = $('#id').val();
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/'; //글이 등록이 성공하면 메인페이지로(/)로 이동
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
 
 };
 
-index.init();
+main.init();
