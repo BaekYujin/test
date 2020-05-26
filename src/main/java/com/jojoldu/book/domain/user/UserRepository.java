3134e4ea0783be7913cb3521b1ec4d3c5bf5c2aa
BaekYujin <byujin66@gmail.com>
2020-05-26 21:52:58 +0900

    스프링 시큐리티 oauth 설정 및 작업 - 1_20200526

---
+++ src/main/java/com/jojoldu/book/domain/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByEmail(String email);
+    //findByEmail 소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 판단하기 위한 메소드 입니다.
+}
